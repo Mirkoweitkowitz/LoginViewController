be7a64045e47b2df339a6adba3f7ee2878537ba8
Mirkoweitkowitz <99251369+Mirkoweitkowitz@users.noreply.github.com>
2022-08-19 12:35:40 +0200

    19.08.22 1 commit alles erledigt

--- LoginViewController/ViewController.swift
+++ LoginViewController/ViewController.swift
@@ -9,11 +9,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var benutzerlabel: UILabel!
+    
+    @IBOutlet weak var benutzerinput: UITextField!
+    
+    @IBOutlet weak var passwordlabel: UILabel!
+    
+    @IBOutlet weak var passwordinput: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func show(_ sender: UIButton) {
+        benutzerlabel.isHidden = false
+        passwordlabel.isHidden = false
+        
+        benutzerlabel.text = benutzerinput.text
+        
+        passwordlabel.text = passwordinput.text
+        benutzerinput.text = ""
+        passwordinput.text = ""
+    }
+    
+    @IBAction func clear(_ sender: UIButton) {
+        benutzerlabel.isHidden = true
+        passwordlabel.isHidden = true
+        
+        benutzerlabel.text = ""
+        passwordlabel.text = ""
+    }
 }
 
